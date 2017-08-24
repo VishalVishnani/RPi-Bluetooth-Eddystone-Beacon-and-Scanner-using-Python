afae5bb6ba3250f1b6b1aab129d4755815736455
Vishal Vishnani <vishalvishnani30@gmail.com>
2017-08-24 16:14:24 -0600

    Edited code to test scanner

--- Beacon_Receiver/test_ble_scanner.py
+++ Beacon_Receiver/test_ble_scanner.py
@@ -1,3 +1,4 @@
+# This code calls the functions defined in ble_scanner.py which scans beacons and if beacon is recognized eddystone beacons then it decodes the information from the packet
 
 import ble_scanner
 import sys
@@ -18,6 +19,7 @@ ble_scanner.hci_le_set_scan_parameters(sock)
 ble_scanner.hci_enable_le_scan(sock)
 
 while True:
+	# Fuction to scan beacons
 	ble_scanner.parse_events(sock, 10)
 	#print("--------------------------------------")
 
